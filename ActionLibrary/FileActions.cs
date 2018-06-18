a0574fb49f506c167ec12336f3dd391aeb2fa5b5
MultiDream <luka.derado@gmail.com>
2018-06-18 12:55:03 -0400

    initial commit

---
+++ ActionLibrary/FileActions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace FileActions
+{
+    public class Command{
+
+		/* Writes to a location. */
+		public static void writeFile(String path,params object[] args) {
+			System.Console.Write("Writing...");
+			try {
+				String toWrite = (String) args[0];//Converts args[0] into a string.
+				using (FileStream fs = File.Open(path, FileMode.Append ))
+				using (TextWriter writer = new StreamWriter(fs)) {
+					writer.WriteLine(toWrite);
+				}
+				System.Console.WriteLine("Successful.\n");
+			} catch (Exception specifics) {
+				Console.WriteLine("Failed!");
+				Console.WriteLine("Unknown error occured when dating {0}",path);
+				throw specifics;
+			}
+		}
+
+		/* Clears a file. */
+		public static void clearFile(String path, params object[] args) {
+			System.Console.Write("Clearing...");
+			try {
+				File.WriteAllText(path,null);
+				System.Console.WriteLine("Successful.\n");
+			} 
+			/* File Opened. */
+			catch (System.IO.IOException specifics) {
+				Console.WriteLine("Failed!");
+				Console.WriteLine("File in use. Make sure the file is not open anywhere, and try again!");
+				throw specifics;
+			}
+			/* Abort. */
+			catch (Exception specifics) {
+				Console.WriteLine("Failed!");
+				Console.WriteLine("Unknown error occured when clearing {0}\n", path);
+				throw specifics;
+			}
+		}
+
+		/* Reads a File. */
+		public static void readFile(String path, params object[] args) {
+			System.Console.Write("Reading...\n\n");
+			try {
+				using (FileStream fs = File.Open(path, FileMode.Open))
+				using (TextReader reader = new StreamReader(fs)) {
+					while(reader.Peek() > -1) {
+						Console.WriteLine(reader.ReadLine());
+					}
+				}
+				System.Console.WriteLine("\nDone Reading.\n");
+			} catch (Exception specifics) {
+				Console.WriteLine("Failed!");
+				Console.WriteLine("Unknown error occured when reading {0}", path);
+				throw specifics;
+			}
+		}
+	}
+}
---
+++ ConsoleProvinceEditor/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using FileActions;
+
+namespace ConsoleProvinceEditor {
+	class Program {
+		//Directory containing province files.
+		public const String dir = @"C:\Users\lcderado\Documents\TextPlayground";
+		static void Main(string[] args) {
+			bool running = true;
+			String command = "";
+			while (running) {
+				//Retrieve User Command
+				Console.WriteLine(">> Enter a command...");
+				String input = Console.ReadLine();
+				Console.WriteLine("your command was:\n{0}\n",input);
+
+				//Interpret Command.
+				inp
+				//Switch statement selects command.
+				switch (input) {
+					case "quit":
+						Console.WriteLine("Quiting.");
+						running = false;
+						break;
+				}
+			}
+		}
+		public static void testBuild(String buildDir) {
+			for (int i = 1; i < 1000; i++) {
+				String path = buildDir + "\\" + i + " - TestFile_" + i + ".txt";
+				File.Create(path);
+			}
+		}
+	}
+
+	/*
+	 * Console always need to grab from the same place, all that changes
+	 * is what happens once they find the right file.
+	 * 
+	 * A Console can perform a command.
+	 * */
+	public class CommandConsole {
+		public String ActDir {get; set;}
+		private int[] members;
+		public delegate void Plan(String path, params object[] arguments); //What to do once you get to a file.
+		public CommandConsole(String actDir,int[] members) {
+			ActDir = actDir;
+			this.members = members;
+		}
+		public void Go(Plan function, params object[] args) {
+			//wraps around call.
+			String[] files = Directory.GetFiles(ActDir);
+			int[] targets = members;
+			int countMiss = 0;
+			foreach (int target in targets) {
+				String pattern = @"\\" + target.ToString() + @"\s";
+				bool exists = false;
+				foreach (string path in files) {
+					if (Regex.Match(path, pattern).Success) {
+						Console.WriteLine("Member {0} found...",path);
+						function(path, args);
+						exists = true;
+						break;
+					}
+				}
+				if (!exists) {
+					Console.WriteLine("Member {0} not found.", target);
+					countMiss++;
+				}
+			}
+			if (countMiss > 0) {
+				Console.WriteLine("Missing {0} files!", countMiss);
+			}
+		}
+	}
+}
